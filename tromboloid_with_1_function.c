79f0b803090e7b6163f5c282710ad4f073b78f1d
ENG19CS0039-AnushreeMitra <76113643+ENG19CS0039-AnushreeMitra@users.noreply.github.com>
2021-02-03 17:47:03 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -1,12 +1,13 @@
+#include<stdio.h>
 int main()
 {
 	float h, d, b, vol;
 	printf("Enter the values of h, d and b respectively: ");
-	scanf("%f %f %f ", &h,&d,&b);
+	scanf("%f%f%f",&h,&d,&b);
 	float x,y;
 	x = h*d*b;
 	y = d/b;
-	vol = (1/3)*(x+y);
+	vol = (x+y)*1/3;
 	printf("The volume of tromboloid for h = %f, d = %f and b = %f is %f unit cube.\n",h,d,b,vol);
 	return(0);
 }
