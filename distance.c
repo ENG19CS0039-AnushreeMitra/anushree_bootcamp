097869cf478e36164e8da11c3f1b0125c28d7db6
ENG19CS0039-AnushreeMitra <76113643+ENG19CS0039-AnushreeMitra@users.noreply.github.com>
2021-02-08 11:09:09 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,31 +1,35 @@
 #include<stdio.h>
 #include<math.h>
+int x1,x2;
+int y, y2;
 
 void input()
 {
-	int x1, x2, y1, y2;
-printf("Enter the (x,y) coordinates of point 1: ");
-	scanf("%d%d",&x1,&y1);
+    printf("Enter the (x,y) coordinates of point 1: ");
+	scanf("%d%d",&x1,&y);
 	printf("Enter the (x,y) coordinates of point 2: ");
 	scanf("%d%d", &x2, &y2);
 }
+
 void disp()
 {
-	float d = dist(x1, x2, y1, y2) ;
-	printf("The distance between (%d,%d) and (%d,%d) is %f units\n", x1,y1,x2,y2,d);
+	float distance;
+	distance = dist() ;
+	printf("The distance between (%d,%d) and (%d,%d) is %f units\n", x1,y,x2,y2,distance);
 }
-float dist(int x1, int x2, int y1, int y2)
+int dist()
 {
+
 	float d, num, denom;
 	num = pow((x1-x2),2);
-	denom = pow((y1-y2),2);
+	denom = pow((y-y2),2);
 	d = sqrt(num+denom);
 	return d;
 }
 int main()
 {
 	input();
-	display();
+	disp();
 	return 0;
 }
-	
+
