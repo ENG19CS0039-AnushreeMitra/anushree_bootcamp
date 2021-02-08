d8cebee304ab0556e1bfcd256b09b8cffe1177a4
ENG19CS0039-AnushreeMitra <76113643+ENG19CS0039-AnushreeMitra@users.noreply.github.com>
2021-02-08 20:35:33 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,1 +1,19 @@
-//WAP to find the distance between two points using structures and 4 functions.
+#include<stdio.h>
+#include<math.h>
+struct cords
+{ 
+	int x,y;
+}c1, c2;
+
+int main()
+{ 
+	float distance, xco, yco;
+printf("Enter the coordinates for point 1: ");
+	scanf("%d%d",&c1.x, &c1.y);
+	printf("Enter the coordinates for point 2: ");
+	scanf("%d%d", &c2.x,c2.y);
+	xco = pow((c1.x-c2.x),2);
+	yco = (pow((c1.y-c2.y),2);
+	distance = sqrt(xco+yco);
+	printf("The distance between (%d,%d) and (%d,%d) is %f units\n",c1.x, c1.y, c2.x, c2.y,,distance);
+}
