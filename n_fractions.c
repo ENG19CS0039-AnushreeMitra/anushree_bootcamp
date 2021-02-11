197a4b82c30980bfbd868a59f9afe04966f75791
ENG19CS0039-AnushreeMitra <76113643+ENG19CS0039-AnushreeMitra@users.noreply.github.com>
2021-02-11 11:03:49 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,31 @@
-//WAP to find the sum of n fractions.
+#include<stdio.h>
+struct frac
+{
+	int num, denom;
+};
+int main()
+{
+	struct fract x[100];
+	int n, i,gcd, nsum= 0, dval= 1;
+	printf("Enter the value of 'n': ");
+	scanf("%d", &n);
+	for(i=0; i<n; i++)
+	{
+		printf("Enter the numerator of fraction %d: ", i+1);
+		scanf("%d", &x[i].num);
+		printf("Enter the denominator of fraction %d: ", i+1);
+		scanf("%d", &x[i].denom);
+		printf("The entered fraction is %d/%d\n", x[i].num, x[i].denom);
+		nsum += x[i].num;
+		dval *= x[i].denom;
+	}
+	//for gcd:
+	for(i=1; i<=nsum && i<=dval; i++)
+	{
+		if((nsum%i == 0) && (dval%i == 0))
+			gcd = i;
+	}
+	nsum /= gcd; dval /= gcd;
+	printf("The sum of given %d fractions is %d/%d\n",n,nsum,dval);
+	return 0;
+}
