af35db31f6791b67b47245cb1cf256e979b3a091
ENG19CS0039-AnushreeMitra <76113643+ENG19CS0039-AnushreeMitra@users.noreply.github.com>
2021-02-23 15:20:09 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -5,18 +5,29 @@ struct cords
 	int x,y;
 }c1, c2;
 
-int main()
+void ip()
 {
-	float distance;
-	int xco, yco;
-    printf("Enter the coordinates for point 1: ");
+printf("Enter the coordinates for point 1: ");
 	scanf("%d%d",&c1.x, &c1.y);
 	printf("Enter the coordinates for point 2: ");
 	scanf("%d%d", &c2.x,&c2.y);
 	printf("%d %d %d %d\n", c1.x,c1.y, c2.x, c2.y);
+}
+
+void disp(float d)
+{
+	printf("The distance between (%d,%d) and (%d,%d) is %f units\n",c1.x, c1.y, c2.x, c2.y,d);
+}
+
+int main()
+{
+	float distance;
+	int xco, yco;
+	ip();
 	xco = pow((c1.x-c2.x),2);
 	yco = pow((c1.y-c2.y),2);
 	distance = sqrt(xco+yco);
-	printf("The distance between (%d,%d) and (%d,%d) is %f units\n",c1.x, c1.y, c2.x, c2.y,distance);
+	disp(distance);
 	return 0;
 }
+
